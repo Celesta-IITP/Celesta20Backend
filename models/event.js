205d6a87d136e50d6cc2a6a093c73439ee9c8a8c
Aman Jee <amangrobo@gmail.com>
2020-03-02 02:34:01 +0530

    Update event.js

--- models/event.js
+++ models/event.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-    relatedClub: {
+    addedBy: {
         type: Schema.Types.ObjectId,
-        ref: 'club',
+        ref: 'user',
         required: true
     },
-    venue: {
-        type: String,
-        required: true
-    },
+    organizers: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }],
+
     name: {
         type: String,
         required: true
@@ -22,14 +23,63 @@ const eventSchema = new Schema({
     imageUrl: {
         type: String
     },
-    date: {
-        type:Number, 
+    thumbnailUrl: {
+        type: String
+    },
+
+    venue: {
+        type: String,
         required: true
     },
-    postLinks : {
-        type : Array , 
-        "default" : []
+    venueUrl: {
+        type: String,
+        required: true
     },
+    date: {
+        type: String, 
+        required: true
+    },
+    startTime: {
+        type: String
+    },
+    endTime: {
+        type: String
+    },
+    teamSize: {
+        type: Number,
+        default: 1
+    },
+
+    rulebookUrl: {
+        type: String
+    },
+    registrationUrl: {
+        type: String
+    },
+    registrationCharge: {
+        type: Number,
+        default: 0
+    },
+
+    postLinks : [{
+        type : String
+    }],
+
+    registrations: [{
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+            required: true
+        },
+        orderId: {
+            type: String
+        },
+        paymentId: {    //or transactionId
+            type: String
+        },
+
+    }]
+
     
 });
 
